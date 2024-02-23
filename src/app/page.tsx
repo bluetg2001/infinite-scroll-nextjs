1d466e87393b398d10a3ddcf8dd7b3510fe71adb
cotist <tg.kim@alcherainc.com>
2024-02-23 14:19:02 +0900

    feat: making infinite scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,17 @@ import BeerCard from "@/components/BeerCard";
 import { Beer } from "@/types";
 import { Heading, VStack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 function Home() {
   const [beers, setBeers] = useState<Beer[] | null>(null);
+  const [page, setPage] = useState(1);
+
+  const { ref, inView } = useInView();
 
   useEffect(() => {
     const init = async () => {
-      await fetchBeers(1).then((data) => {
+      await fetchBeers(page).then((data) => {
         setBeers(data);
       });
     };
@@ -19,16 +23,35 @@ function Home() {
     init();
   }, []);
 
+  useEffect(() => {
+    if (inView) {
+      handleLoadMoreData();
+    }
+  }, [inView]);
+
+  const delay = (ms: number) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
+  const handleLoadMoreData = async () => {
+    await delay(1000);
+    const nextPage = page + 1;
+    await fetchBeers(nextPage).then((data) => {
+      setPage(page + 1);
+      setBeers((prevProducts) => [...prevProducts, ...data]);
+    });
+  };
+
   return (
     <VStack w={"100%"}>
       <Heading mt={8}>Example Infinite Scroll</Heading>
       {beers ? (
-        beers.map(() => {
-          return <BeerCard />;
+        beers.map((item: Beer) => {
+          return <BeerCard item={item} />;
         })
       ) : (
         <></>
       )}
+      <div ref={ref}></div>
     </VStack>
   );
 }
--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -1,17 +1,21 @@
 import { Beer } from "@/types";
-import { Card, CardBody, CardHeader, Text } from "@chakra-ui/react";
+import { Card, CardBody, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
 export interface BeerProps {
   beers: Beer[] | null;
 }
 
-function BeerCard() {
+function BeerCard(props) {
+  const { item } = props;
+
   return (
     <Card>
-      <CardHeader></CardHeader>
       <CardBody>
-        <Text>Bye</Text>
+        <VStack>
+          <Text>{item.id}</Text>
+          <Text>{item.name}</Text>
+        </VStack>
       </CardBody>
     </Card>
   );
